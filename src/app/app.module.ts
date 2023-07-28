acd3a6ddd9572a0c4ffcea804cafb4b9d8419dc8
Orsi Suto <orsolya.suto.so@gmail.com>
2023-07-28 12:22:40 +0200

    autogenerated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrawingComponent } from './components/drawing/drawing.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { QuestionComponent } from './components/question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawingComponent,
+    KeyboardComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
