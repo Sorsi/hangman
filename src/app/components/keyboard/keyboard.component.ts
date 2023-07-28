6e8a83d2003099a5c9573365c25cee9a95ab29ef
Orsi Suto <orsolya.suto.so@gmail.com>
2023-07-28 17:11:25 +0200

    updated keyboard component

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ICharacter } from 'src/app/interfaces/interfaces';
+import { ALPHABET } from 'src/assets/alphatbet';
 
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.html',
-  styleUrls: ['./keyboard.component.scss']
+  styleUrls: ['./keyboard.component.scss'],
 })
 export class KeyboardComponent implements OnInit {
+  @Input() question: string;
+  @Output() keyPressed = new EventEmitter<string>();
 
-  constructor() { }
+  keys: ICharacter[];
 
-  ngOnInit(): void {
+  constructor() {
+    this.question = '';
+    this.keys = [];
   }
 
+  ngOnInit(): void {
+    this.keys = ALPHABET.map((key) => {
+      return {
+        value: key,
+        guessed: false,
+      };
+    });
+  }
+
+  onKeyClick(key: ICharacter): void {
+    if (key.guessed) {
+      return;
+    }
+    key.guessed = true;
+    this.keyPressed.emit(key.value);
+  }
 }
