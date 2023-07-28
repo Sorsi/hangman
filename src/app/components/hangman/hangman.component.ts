a10bc3b1202e0fdd052b281a2f4767e2fd393a2e
Orsi Suto <orsolya.suto.so@gmail.com>
2023-07-28 13:59:43 +0200

    get a random question

--- src/app/components/hangman/hangman.component.ts
+++ src/app/components/hangman/hangman.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { HangmanService } from 'src/app/services/hangman.service';
 
 @Component({
   selector: 'app-hangman',
   templateUrl: './hangman.component.html',
-  styleUrls: ['./hangman.component.scss']
+  styleUrls: ['./hangman.component.scss'],
 })
 export class HangmanComponent implements OnInit {
+  question: string;
+  questions: string[];
+  guesses: string[];
+  category: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private hangmanService: HangmanService) {
+    this.question = '';
+    this.questions = [];
+    this.guesses = [];
+    this.category = '';
   }
 
+  ngOnInit(): void {
+    this.hangmanService.getQuestions().subscribe((res) => {
+      console.log(res);
+
+      this.questions = res.items;
+      this.category = res.category;
+      this.question = this.getNewQuestion();
+      console.log('qqqq ', this.question);
+    });
+  }
+
+  getNewQuestion(): string {
+    return this.questions[this.getRandomIndex()];
+  }
+
+  getRandomIndex(): number {
+    return Math.floor(Math.random() * this.questions.length);
+  }
+
+  reset(): void {
+    this.guesses = [];
+    this.getNewQuestion();
+  }
 }
