a78e30d9eb70c5fafe3d5d654fb62c15859b68e0
Orsi Suto <orsolya.suto.so@gmail.com>
2023-07-28 13:59:05 +0200

    get a random question from data with http

--- src/app/services/hangman.service.ts
+++ src/app/services/hangman.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const defaultJSONPath = 'assets/data.json';
+export interface IQuestions {
+  category: string;
+  items: string[];
+}
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +14,6 @@ export class HangmanService {
   constructor(private http: HttpClient) {}
 
   getQuestions(jsonPath: string = defaultJSONPath) {
-    return this.http.get('api/questions');
+    return this.http.get<IQuestions>(jsonPath);
   }
 }
